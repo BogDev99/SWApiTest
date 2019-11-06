959ce2090fc9a191d307b25723a6cca2f87ecbd5
ihot <lbrdev.contact@gmail.com>
2019-11-06 23:43:33 +0200

    update to viewController and clean  up

--- SWApiTest/SWApiTest/SWListViewController.swift
+++ SWApiTest/SWApiTest/SWListViewController.swift
@@ -8,23 +8,24 @@
 
 import UIKit
 
-
-class SWListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    let apiURL = "https://swapi.co/api/starships"
+class SWListViewController: UIViewController {
     
-    
+    @IBOutlet private weak var tableView: UITableView!
 
+    private let apiURL = "https://swapi.co/api/starships"
+    private var content: [Starship] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchData ()
+        fetchData()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
-    private var content: [Starship] = []
-
-    func fetchData () {
+    private func fetchData() {
         request { (data, error) in
             let decoder = JSONDecoder()
             guard let data = data else { return }
@@ -39,8 +40,7 @@ class SWListViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
     }
 
-    func request(completion: @escaping (Data?, Error?) -> Void) {
-
+    private func request(completion: @escaping (Data?, Error?) -> Void) {
         guard let url = URL(string: apiURL) else { return }
         let request = URLRequest(url: url)
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
@@ -50,7 +50,9 @@ class SWListViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
         task.resume()
     }
-    
+}
+
+extension SWListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
         cell.textLabel?.text = content[indexPath.row].name
@@ -66,6 +68,7 @@ class SWListViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
+        // TODO: -
         print(content[indexPath.row])
     }
 }
